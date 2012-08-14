a794a3d1632a5bbc510f4db771889d83277924ef
Aleksandr Zykov <alexandrz@gmail.com>
2012-08-14 17:10:31 +0600

    retryable

--- tasks/lj_rnd_photo.rb
+++ tasks/lj_rnd_photo.rb
@@ -1,23 +1,36 @@
 desc "Posts random photo tagged with TAG to LJ_JOURNAL (using LJ_LOGIN, LJ_PASSWORD, FLICKR_API_KEY and FLICKR_SECRET)"
 task :lj_rnd_photo do
 
+  def retryable
+    begin
+      return yield
+    rescue
+      retry
+    end
+  end
+
   FlickRaw.api_key=ENV['FLICKR_API_KEY']
   FlickRaw.shared_secret=ENV['FLICKR_SECRET']
 
-  photo = flickr.photos.search(:tags => ENV['TAG'], :per_page => 1, :page => rand(4001)+1)[0]
+  retryable do
+    photo = flickr.photos.search(:tags => ENV['TAG'], :per_page => 1, :page => rand(4001)+1)[0]
+  end
 
-  user = LiveJournal::User.new(ENV['LJ_LOGIN'], ENV['LJ_PASSWORD'])
-  login = LiveJournal::Request::Login.new(user)
-  
-  login.run
+  retryable do
+    user = LiveJournal::User.new(ENV['LJ_LOGIN'], ENV['LJ_PASSWORD'])
+    login = LiveJournal::Request::Login.new(user)
+    login.run
+  end
 
   user.usejournal = ENV['LJ_JOURNAL']
 
   entry = LiveJournal::Entry.new
   entry.subject = ''
   entry.event = "<img src='#{FlickRaw.url_b(photo)}' width='100%'>"
-  request = LiveJournal::Request::PostEvent.new user, entry
 
-  request.run
+  retryable do
+    request = LiveJournal::Request::PostEvent.new user, entry
+    request.run
+  end
 
 end
